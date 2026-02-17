2cf24d511eb8e2e9dd69ee452f3d2b43cf7f36d7
Deepankar Sharma <youremail@example.com>
2026-02-17 09:12:43 +0530

    Added instagram logo

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,6 +6,15 @@ const Footer = () => {
             <div className="container mx-auto px-4 text-center">
                 <p className="mb-2">&copy; Mela {new Date().getFullYear()} FoodPreorder System. </p>
                 <p className="mb-2"> All rights reserved by HotChicksOnly</p>
+                <a
+                    href="https://www.instagram.com/hotchicks.only/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex justify-center items-center gap-2 hover:text-pink-500 transition duration-300"
+                >
+                    <Instagram size={22} />
+                    <span>Follow us on Instagram</span>
+                </a>
             </div>
         </footer>
     );
